ff1a22b5b98bd0bc8df72b4787ff0f8ee1cd34ab
mick nice <mikemccullagh@outlook.com>
2023-10-31 03:40:55 +0000

    adds users data, db queries to create users, product and review tables, inserts product and user data into tables, TODO: insert reviews and references to users

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -5,111 +5,96 @@ const {
   createRef,
   formatComments,
 } = require('./utils');
-const Corpus = require('../../utils/Corpus');
-const articles = require('../data/test-data/articles');
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
+const seed = (userData, productData, commentData ) => {
   return db
-    .query(`DROP TABLE IF EXISTS comments;`)
+    .query(`DROP TABLE IF EXISTS reviews;`)
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS articles;`);
+      return db.query(`DROP TABLE IF EXISTS products;`);
     })
     .then(() => {
       return db.query(`DROP TABLE IF EXISTS users;`);
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS topics;`);
-    })
-    .then(() => {
-      const topicsTablePromise = db.query(`
-      CREATE TABLE topics (
-        slug VARCHAR PRIMARY KEY,
-        description VARCHAR
-      );`);
-
       const usersTablePromise = db.query(`
       CREATE TABLE users (
-        username VARCHAR PRIMARY KEY,
-        name VARCHAR NOT NULL,
-        avatar_url VARCHAR,
-        password VARCHAR NOT NULL
+        id SERIAL PRIMARY KEY,
+        username VARCHAR NOT NULL,
+        first_name VARCHAR NOT NULL,
+        last_name VARCHAR NOT NULL,
+        email VARCHAR NOT NULL,
+        address_line_1 VARCHAR NOT NULL,
+        address_line_2 VARCHAR NOT NULL,
+        city VARCHAR NOT NULL,
+        country VARCHAR NOT NULL,
+        postcode VARCHAR NOT NULL,
+        password VARCHAR NOT NULL DEFAULT 'Password1!'
       );`);
 
-      return Promise.all([topicsTablePromise, usersTablePromise]);
+      return Promise.all([usersTablePromise]);
     })
     .then(() => {
       return db.query(`
-      CREATE TABLE products (
+      CREATE TABLE product (
         product_id SERIAL PRIMARY KEY,
+        long_description TEXT[],
         name VARCHAR NOT NULL,
-        topic VARCHAR NOT NULL REFERENCES topics(slug),
-        author VARCHAR NOT NULL REFERENCES users(username),
-        body VARCHAR NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW(),
-        votes INT DEFAULT 0 NOT NULL,
-        article_img_url VARCHAR DEFAULT 'https://images.pexels.com/photos/97050/pexels-photo-97050.jpeg?w=700&h=700',
-        similar_article INT DEFAULT 0
+        category VARCHAR NOT NULL,
+        sub_category VARCHAR NOT NULL,
+        short_description TEXT NOT NULL,
+        rating INT DEFAULT 0 NOT NULL,
+        img VARCHAR DEFAULT 'https://images.pexels.com/photos/97050/pexels-photo-97050.jpeg?w=700&h=700'
       );`);
     })
     .then(() => {
       return db.query(`
-      CREATE TABLE comments (
-        comment_id SERIAL PRIMARY KEY,
-        body VARCHAR NOT NULL,
-        article_id INT REFERENCES articles(article_id) NOT NULL,
+      CREATE TABLE reviews (
+        id SERIAL PRIMARY KEY,
         author VARCHAR REFERENCES users(username) NOT NULL,
-        votes INT DEFAULT 0 NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW()
+        date TIMESTAMP DEFAULT NOW()
+        title VARCHAR NOT NULL,
+        rating NUMERIC NOT NULL,
+        body VARCHAR NOT NULL,
+        product_id INT REFERENCES products(product_id) NOT NULL
       );`);
     })
     .then(() => {
-      const insertTopicsQueryStr = format(
-        'INSERT INTO topics (slug, description) VALUES %L;',
-        topicData.map(({ slug, description }) => [slug, description])
-      );
-      const topicsPromise = db.query(insertTopicsQueryStr);
+      
       const insertUsersQueryStr = format(
-        'INSERT INTO users ( username, name, avatar_url, password) VALUES %L;',
-        userData.map(({ username, name, avatar_url, password }) => [
-          username,
-          name,
-          avatar_url,
-          password,
+        'INSERT INTO users ( username, first_name, last_name, email, address_line_1, address_line_2, city, country, postcode, password) VALUES %L;',
+        userData.map(({ username, first_name, last_name, email, address_line_1, address_line_2, city, country, postcode, password }) => [
+          username, 
+          first_name, 
+          last_name, 
+          email, 
+          address_line_1, 
+          address_line_2, 
+          city, 
+          country, 
+          postcode, 
+          password
         ])
       );
       const usersPromise = db.query(insertUsersQueryStr);
 
-      return Promise.all([topicsPromise, usersPromise]);
+      return Promise.all([usersPromise]);
     })
     .then(() => {
-      const formattedData = articleData.map(convertTimestampToDate);
-      const formattedArticleData = [...formattedData.map(article => article = {...article})]
-      const bodyArr = [...formattedData.map(article => article = article.body)]
-      const idArr = [...formattedData.map((article, i) => article = i+1)]
-      const corpus = new Corpus(idArr, bodyArr)
-      
-      for(article of formattedArticleData) {   
-        article.similar_article = corpus.getResultsForQuery(article.body)[1][0]                
-      }
-
-      const insertArticlesQueryStr = format(
-        'INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url, similar_article) VALUES %L RETURNING *;',
-        formattedArticleData.map(
+      const insertProductsQueryStr = format(
+        'INSERT INTO product (long_description, name, category, sub_category, short_description, rating, img) VALUES %L RETURNING *;',
+        productData.map(
           ({
-            title,
-            topic,
-            author,
-            body,
-            created_at,
-            votes = 0,
-            article_img_url,
-            similar_article,
-          }) => [title, topic, author, body, created_at, votes, article_img_url, similar_article]
+            description,
+            name,
+            category,
+            sub_cat,
+            overview,
+            rating = 1.0,
+            img,
+          }) => [description, name, category, sub_cat, overview, rating = 1.0, img]
         )
       );
-      
-
-      return db.query(insertArticlesQueryStr);
+      return db.query(insertProductsQueryStr);
     })
     .then(({ rows: articleRows }) => {
       const articleIdLookup = createRef(articleRows, 'title', 'article_id');
